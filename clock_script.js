1bf9b80a33378d37e5c8a746a752ed4f4e9d3e2d
Shatabdi Barua <u1404023@student.cuet.ac.bd>
2023-04-06 02:48:04 +0600

    created digital clock

---
+++ clock_script.js
@@ -0,0 +1,32 @@
+setInterval(showTime, 1000);
+function showTime()
+{
+    const dt = new Date();
+    let hrs = dt.getHours();
+    let mins = dt.getMinutes();
+    let sec = dt.getSeconds();
+    let session = hrs >= 12 ? "PM":"AM" 
+
+    hrs = hrs == 0 ? "12" :( hrs > 12 ? (hrs - 12) : hrs); 
+    hrs = (hrs < 10) ? "0" + hrs : hrs;
+    mins = (mins < 10) ? "0" + mins : mins;
+    sec = (sec < 10) ? "0" + sec : sec;
+
+    let time = hrs + ":" + mins + ":" + sec + " " + session
+    document.querySelector(".show_time").innerHTML = time
+}
+function showDate()
+{
+    const dt = new Date()
+    const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    const months = ["Jan", "Feb", "Mar", "Apr", "May", "June", "July", "Aug", "Sep", "Oct", "Nov", "Dec"];
+
+    let day = days[dt.getDay()]
+    let month = months[dt.getMonth()]
+    let full_date = dt.getDate();
+    let year = dt.getFullYear();
+
+    let current_date = day + ", " + month + " " + full_date + ", " + year;
+    return current_date;
+}
+document.querySelector(".show_date").innerHTML = showDate();
